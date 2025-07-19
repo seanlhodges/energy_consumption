e6c8acb334a0e6869dcc65f5fd4adec85dda31f5
seanhodges1 <sean.hodges.1@gmail.com>
2025-07-20 05:21:39 +1200

    Data update 2025-07-18

--- rollback_parquet.py
+++ rollback_parquet.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
-df = pd.read_parquet("gas_usage.parquet")
+df = pd.read_parquet("electricity_usage.parquet")
 
 print(df['YYYYMMDD'].max())
 
 output_df = df[df['YYYYMMDD'] != df['YYYYMMDD'].max()]
 print(output_df['YYYYMMDD'].max())
 
-output_df.to_parquet("gas_usage.parquet", index=False)
+output_df.to_parquet("electricity_usage.parquet", index=False)
